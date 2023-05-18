fb16a8afa806015fd7f8b39e850d4efe510180ee
anshulme96@gmail.com <anshulme96@gmail.com>
2023-05-18 21:16:31 +0530

    coreConceptOfJS

---
+++ coreCocept.js
@@ -0,0 +1,16 @@
+const name="Anshul";
+let age=27;
+const hasHobbies=true;
+
+function userDetails(userName,uaerAge,userHobbies){
+
+    return ("nmae is "+userName+" age "+age + " hashobbies" +userHobbies);
+}
+
+console.log(userDetails( name,age,hasHobbies));
+
+const multiply=(a,b)=>{
+    return a*b;
+}
+
+console.log(multiply(3,4));
