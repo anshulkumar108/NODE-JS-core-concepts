69bab49c385ddf77a4e9d5529881b610199dee5b
anshulme96@gmail.com <anshulme96@gmail.com>
2023-05-21 15:36:14 +0530

    getting first response

--- createServer.js
+++ createServer.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-const server=http.createServer((res,req)=>{
-    console.log("Anshul kumar")
+const server=http.createServer((req,res)=>{
+    //console.log("Anshul kumar")
+    //console.log(req.url,  req.method , req.headers)
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head> <title>Node Js project</title></head>')
+    res.write('<h2>Welcome to my Node Js project</h2>')
+    res.write('</html>')
+    res.end();
+
 })
 
 server.listen(4000);
