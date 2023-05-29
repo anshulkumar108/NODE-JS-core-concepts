c5f61e2af64c8c2a29f2a7089f1ec25f0716e394
ashulme96 <anshulme96@.com>
2023-05-29 07:18:08 +0530

    intro to middleware

--- chatApp.js
+++ chatApp.js
@@ -1,20 +1,35 @@
-const http = require('http');
+//const http = require('http');
+const bodyParser = require('body-parser');
 
-const  express= require('express')
+// const routes=require('./route.js');
+// console.log(routes.someText);
 
-const app=express()//The express() function is a top-level function exported by the express module.
-
-app.use((req,res,next)=>{
-    console.log("in the middleware")
-    next();
-})
-
-app.use((req,res,next)=>{
-   // console.log("in the another middleware")
-   res.send('<h1> hello to node js </h1>')
-})
-
-// const server = http.createServer(app);
+// const server = http.createServer(routes.handler);
 
 // server.listen(4000);
+const express=require('express');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST" ><input type="text" name="title"><input type="text" name="productSize"><button type="submit">add product</button></form>')
+})
+
+//this will run for both post and get requests
+// app.use('/product',(req,res,next)=>{
+// console.log(req.body); //by default this body request does not parse incoming body request to do this we need to add parser
+// res.redirect('/');
+// })
+
+//this will run for only get request
+app.post('/product',(req,res,next)=>{
+    console.log(req.body); //by default this body request does not parse incoming body request to do this we need to add parser
+    res.redirect('/');
+    })
+
+app.use('/',(req,res,next)=>{
+     res.send('<h1>hello from express js</h1>');
+ })
+ 
 app.listen(4000)
